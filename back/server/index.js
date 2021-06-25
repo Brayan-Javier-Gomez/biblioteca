33959adc6b7f829e1ee2892fa65593a672697d16
brayan <brayanjaviergomez.g@gmail.com>
2021-06-25 13:23:27 -0500

    Archivo demo de Produccion

--- back/server/index.js
+++ back/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path')
 const app = express();
 
 
@@ -25,6 +26,16 @@ mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology:
     console.log('Base de datos iniciada puerto 27017');
 
 });
+
+app.use(express.static(__dirname + "/production/biblioteca"));
+
+app.get('*', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname + "/production/biblioteca/index.html"))
+
+});
+
+
 
 
 app.listen(process.env.PORT, () => {
