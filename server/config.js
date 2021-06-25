9a505336c3bf9690182de884c117c36453069511
brayan <brayanjaviergomez.g@gmail.com>
2021-06-25 14:02:47 -0500

    demo production

--- server/config.js
+++ server/config.js
@@ -9,9 +9,9 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/biblioteca';
 
 
-} else {
-
-    urlDB = 'mongodb+srv://bryan_gomez:Brayangomez1986@cluster0.ievef.mongodb.net/biblioteca';
 }
+
+urlDB = 'mongodb+srv://bryan_gomez:Brayangomez1986@cluster0.ievef.mongodb.net/biblioteca';
+
 
 process.env.URLDB = urlDB;
